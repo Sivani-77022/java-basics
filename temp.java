e6d64aab55e63a98e229f9ec64340fe372d83a6f
Sivani-77022 <sivani3004@gmail.com>
2025-07-24 22:47:31 +0530

    Add files via upload
    temperature checking precautions

---
+++ temp.java
@@ -0,0 +1,15 @@
+ 
+ class temp {
+    public static void main(String[] args) {
+        int temp =30;
+        if (temp >45) {
+            System.out.println("Ram should stay home.");
+        }
+        else if (temp >30 && temp >45) {
+            System.out.println("he should carry water.");
+        } 
+        else {
+            System.out.println("he go normally.");
+        }
+    }
+}
