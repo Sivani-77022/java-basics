2a6b5af4da4b4e86d9504482237021df524f7eb8
Sivani-77022 <sivani3004@gmail.com>
2025-07-25 17:02:28 +0530

    health insurance
    conditional statements

---
+++ health.java
@@ -0,0 +1,21 @@
+ 
+ class health {
+    public static void main(String[] args) {
+        int age=20;
+        if (age >60) {
+            System.out.println("premium is 5000.");
+        }
+        else if (age >40 && age <60) {
+            System.out.println("premium is 3000.");
+        } 
+        else if(age>20 && age<39) {
+            System.out.println("premium is 1500.");
+        }
+        else if(age<20){
+
+             System.out.println("premium is 1000.");
+        }  
+
+
+    }
+}
