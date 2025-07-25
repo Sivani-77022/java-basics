5cf3560d3c978d7aa5a808e4e4e879a29f337936
Sivani-77022 <sivani3004@gmail.com>
2025-07-25 18:13:49 +0530

    car services
    conditional statements using if else if statements

---
+++ car.java
@@ -0,0 +1,17 @@
+ 
+ class car{
+	public static void main(String[] args) {
+           int k=1000;
+           if(k>=20000){
+               System.out.println("it needs full service");
+            }
+             else if(k>=10000 && k>20000){
+                System.out.println("it needs semi service");
+             }
+               else{
+                 System.out.println("only general needed");
+
+                  }
+		
+}
+}
