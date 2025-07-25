9005f6a1daaad568effd385e674c9224ecec1c42
Sivani-77022 <sivani3004@gmail.com>
2025-07-25 18:25:50 +0530

    online shopping
    conditional statement

---
+++ online.java
@@ -0,0 +1,16 @@
+ 
+ class online{
+	public static void main(String[] args) {
+          int amount=6000;
+boolean ismember=true;
+           if(amount>=10000 && ismember){
+               System.out.println("30% discount");
+           }
+            else if(amount>=5000 && ismember){
+               System.out.println("20% discount");
+               }
+            else{
+               System.out.println("gets flat 5% discount on any amount");
+             }
+}
+}
