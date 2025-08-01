26b272e579f91e383c913c69457705566f8cf7f0
Sivani-77022 <sivani3004@gmail.com>
2025-08-01 17:28:35 +0530

    area of square
    problem solving

---
+++ area.java
@@ -0,0 +1,10 @@
+ 
+class square {
+    public static void main(String[] args) {
+        int n=5;
+        int Area=n*n;
+        
+        
+        System.out.println("Area of the length is:"+(n*n));
+    }
+}
