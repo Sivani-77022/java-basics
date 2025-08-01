0b6eaaf0c8a4229b5171cb4d457403427d8f002e
Sivani-77022 <sivani3004@gmail.com>
2025-08-01 17:54:50 +0530

    perimeter square
    problem solving

---
+++ perimeter.java
@@ -0,0 +1,10 @@
+class perimeter{
+    public static void main(String[] args) {
+        int side=6;
+ 
+        int peri=4*side;
+        
+        
+        System.out.println("perimeter  of the square is:"+(4*side));
+    }
+}
