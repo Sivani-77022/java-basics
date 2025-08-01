1eb359509eaf381f76c1764035cf826bdbf6247a
Sivani-77022 <sivani3004@gmail.com>
2025-08-01 17:44:41 +0530

    area of triangle
    problem solving

---
+++ triangle.java
@@ -0,0 +1,11 @@
+ 
+class triangle {
+    public static void main(String[] args) {
+        int base=8;
+int height=5; 
+        double Area =0.5*base*height;
+        
+        
+        System.out.println("Area of the triangle is:"+( 0.5*base*height));
+    }
+}
