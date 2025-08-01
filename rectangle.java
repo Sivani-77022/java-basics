7d58091f4429f3f178525ecb14e089c7047a33e9
Sivani-77022 <sivani3004@gmail.com>
2025-08-01 17:32:40 +0530

    area of rectangle
    problem solving

---
+++ rectangle.java
@@ -0,0 +1,11 @@
+ 
+class rectangle {
+    public static void main(String[] args) {
+        int l=5;
+int b=7; 
+        int Area =l*b;
+        
+        
+        System.out.println("Area of the recatangle is:"+(l*b));
+    }
+}
