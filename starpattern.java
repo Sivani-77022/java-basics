6709290a9371f5f34db063e6b7d4cba918c4c7a2
Sivani-77022 <sivani3004@gmail.com>
2025-08-28 21:30:07 +0530

    strings
    patterns

---
+++ starpattern.java
@@ -0,0 +1,9 @@
+public class StarPattern {
+    public static void main(String[] args) {
+        
+        String row = "* * * * *";
+for (int i = 0; i < 4; i++) {
+            System.out.println(row);
+        }
+    }
+}
